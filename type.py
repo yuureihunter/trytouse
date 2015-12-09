d0121fd4983c285a4033038b51beebfd53b184b1
pimpavee <58070101@kmitl.ac.th>
2015-12-09 18:21:02 +0700

    test type

---
+++ type.py
@@ -0,0 +1,15 @@
+"""speed typing"""
+
+def choosetype(number):
+    """print string"""
+    if number == 1:
+        print("An Essay Concerning Human Understanding is a work by \
+John Locke concerning the foundation of human knowledge and understanding.")
+    elif number == 2:
+        print("It first appeared in 1689 (although dated 1690) \
+with the printed title An Essay Concerning Human Understanding.")
+    elif number == 3:
+        print("He describes the mind at birth as a blank slate \
+(tabula rasa, although he did not use those actual words) filled \
+later through experience.")
+choosetype(int(input()))
